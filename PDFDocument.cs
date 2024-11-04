aae4890e5e0c5c113d093dae6dbf94be9a145273
nikolajRoager <nikolaj@ulvedalen.dk>
2024-11-04 14:43:10 +0100

    Added basic structure for PDFDocument class, and debug program for testing  valid and invalid adresses

---
+++ PDFDocument.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PDFDownloader
+{
+    internal class PDFDocument
+    {
+        public string Source;
+        public string? Output;//If null, it will not be saved
+
+        public string Error = "No Error";
+        private byte[] data;//Will be empty if we either skip, or fail
+
+        public enum PdfStatus {NotLoaded/*Has not even tried loading, bytes and error are empty*/, Loaded/*No problem (bytes are not empty, error is empty)*/, Skipped/*Already existed (bytes and error are empty)*/, Failed/*Error included (bytes is empty, error is not)*/};
+
+        public PdfStatus pdfStatus=PdfStatus.NotLoaded;
+
+        public PDFDocument(string source,string? folder, string name, bool force=false/*Overwrite existing files in the output (Keep this false for debugging, that way we quickly get to the ones which failed!)*/)
+        {
+            Source = source;
+            Output = folder == null ? null : Path.Combine(folder,name);
+            pdfStatus = PdfStatus.NotLoaded;
+            Error = "No error";
+
+            data = new byte[0];
+        }
+
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,2 +1,27 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using PDFDownloader;
+using static System.Net.WebRequestMethods;
+
+class Program
+{
+    
+    public static void Main(string[] args)
+    {
+        //Some examples, the comment indicate the expected output
+        string[] urls = {
+            "http://arpeissig.at/wp-content/uploads/2016/02/D7_NHB_ARP_Final_2.pdf",//File not found
+            "http://cdn12.a1.net/m/resources/media/pdf/A1-Umwelterkl-rung-2016-2017.pdf",//Works 
+            "https://s3-eu-west-1.amazonaws.com/a2a-be/a2a/gbb-uploads/N0Go7n-BilancioSostenibilita_EN.pdf",//Does not work, possibly permission denied from server, or invalid pdf
+            "file:///C:/Users/miller/Downloads/AA_AR2017.pdf",//This is not a website url, it doesn't exist.
+            "http://www.hkexnews.hk/listedco/listconews/SEHK/2017/0512/LTN20170512165.pdf",//Works
+            ""//This obviously doesn't exist
+        };
+
+        string folder = "Downloads";
+
+        PDFDocument[] docs = new PDFDocument[urls.Length];
+        for (int i = 0; i < urls.Length; i++)
+        {
+            docs[i]=new PDFDocument(urls[i], folder,$"File{i}.pdf",true);
+        }
+    }
+}
